65cd4b8044101a0aab2187a909392fc3c808dfe4
DrissGreen <driss.janid1@gmail.com>
2014-05-04 02:21:07 +0100

    Creation du module "Clients" Change-Id: Id732339cc7c36e3edb3c245f01e081095bc166da

---
+++ module/Clients/config/module.config.php
@@ -0,0 +1,50 @@
+<?php
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'Clients\Controller\Unclient' => 'Clients\Controller\UnclientController',
+        ),
+    ),
+    'router' => array(
+        'routes' => array(
+            'clients' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    // Change this to something specific to your module
+                    'route'    => '/unclient',
+                    'defaults' => array(
+                        // Change this value to reflect the namespace in which
+                        // the controllers for your module are found
+                        '__NAMESPACE__' => 'Clients\Controller',
+                        'controller'    => 'Unclient',
+                        'action'        => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    // This route is a sane default when developing a module;
+                    // as you solidify the routes for your module, however,
+                    // you may want to remove it and replace it with more
+                    // specific routes.
+                    'default' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '/[:controller[/:action]]',
+                            'constraints' => array(
+                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            ),
+                            'defaults' => array(
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'Clients' => __DIR__ . '/../view',
+        ),
+    ),
+);
---
+++ module/Clients/src/Clients/Controller/UnclientController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/Clients for the canonical source repository
+ * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Clients\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class UnclientController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return array();
+    }
+
+    public function fooAction()
+    {
+        // This shows the :controller and :action parameters in default route
+        // are working when you browse to /unclient/unclient/foo
+        return array();
+    }
+}
