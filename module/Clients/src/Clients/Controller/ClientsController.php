ad7d5ee379603fa5aacc2d083be7c134c1e13fef
DrissGreen <driss.janid1@gmail.com>
2014-05-06 15:59:02 +0100

    test et correction des routes Change-Id: Ie5feaff27dd0e14d9b2b62b8172bfe1b0afb1e7d

--- module/Clients/src/Clients/Controller/ClientsController.php
+++ module/Clients/src/Clients/Controller/ClientsController.php
@@ -1,24 +1,40 @@
 <?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/Clients for the canonical source repository
+ * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
 
-namespace Application\Controller;
+namespace Clients\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
-/**
- * ClientsController
- *
- * @author
- *
- * @version
- *
- */
-class ClientsController extends AbstractActionController {
-	/**
-	 * The default action - show the home page
-	 */
-	public function indexAction() {
-		// TODO Auto-generated ClientsController::indexAction() default action
-		return new ViewModel ();
-	}
+class ClientsController extends AbstractActionController
+{
+    
+ 
+    public function mergeAction()
+    {
+    	$view=new ViewModel();
+    	$view->setTemplate('clients/clients/merge');
+    	return $view;
+    }
+    
+    public function listerAction()
+    {
+    	$view=new ViewModel();
+    	$view->setTemplate('clients/clients/lister.phtml');
+    	return $view;
+    }
+    
+    public function listerNoirAction()
+    {
+    	$view=new ViewModel();
+    	$view->setTemplate('clients/clients/listerNoir.phtml');
+    	return $view;
+    }
+    
 }
