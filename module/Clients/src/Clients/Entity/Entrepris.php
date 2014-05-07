364c2dd5b49a2f1a5d5fd4c4a5027d1d0fe89a40
DrissGreen <driss.janid1@gmail.com>
2014-05-07 22:38:50 +0100

    correction des conflits
    Change-Id: I63b75bf2423032a322d98c255c35b3775a86f0c3
    Signed-off-by: DrissGreen <driss.janid1@gmail.com>

---
+++ module/Clients/src/Clients/Entity/Entrepris.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Clients\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Entrepris
+ *
+ * @ORM\Table(name="entrepris")
+ * @ORM\Entity
+ */
+class Entrepris
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=254, nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="raisonSocial", type="string", length=254, nullable=true)
+     */
+    private $raisonsocial;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="rc", type="string", length=254, nullable=true)
+     */
+    private $rc;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="inter_fin", type="string", length=254, nullable=true)
+     */
+    private $interFin;
+
+
+
+    /**
+     * Get nom
+     *
+     * @return string 
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set raisonsocial
+     *
+     * @param string $raisonsocial
+     * @return Entrepris
+     */
+    public function setRaisonsocial($raisonsocial)
+    {
+        $this->raisonsocial = $raisonsocial;
+    
+        return $this;
+    }
+
+    /**
+     * Get raisonsocial
+     *
+     * @return string 
+     */
+    public function getRaisonsocial()
+    {
+        return $this->raisonsocial;
+    }
+
+    /**
+     * Set rc
+     *
+     * @param string $rc
+     * @return Entrepris
+     */
+    public function setRc($rc)
+    {
+        $this->rc = $rc;
+    
+        return $this;
+    }
+
+    /**
+     * Get rc
+     *
+     * @return string 
+     */
+    public function getRc()
+    {
+        return $this->rc;
+    }
+
+    /**
+     * Set interFin
+     *
+     * @param string $interFin
+     * @return Entrepris
+     */
+    public function setInterFin($interFin)
+    {
+        $this->interFin = $interFin;
+    
+        return $this;
+    }
+
+    /**
+     * Get interFin
+     *
+     * @return string 
+     */
+    public function getInterFin()
+    {
+        return $this->interFin;
+    }
+}
