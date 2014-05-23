d8997bcb7a72995b51015a6eae583bb321d465cb
DrissGreen <driss.janid1@gmail.com>
2014-05-23 22:05:49 +0100

    ajouter agent action et vue

--- module/Parc/src/Parc/Controller/IndexController.php
+++ module/Parc/src/Parc/Controller/IndexController.php
@@ -30,6 +30,20 @@ class IndexController extends AbstractActionController
 	
 	public function ajouterAction()
 	{
+		$form = new UserForm();
+		$request = $this->getRequest();
+		if ($request->isPost()) {
+			$form->setInputFilter(new UserFilter());
+			$form->setData($request->getPost());
+			if ($form->isValid()) {
+				$data = $form->getData();
+				unset($data['submit']);
+				if (empty($data['usr_registration_date'])) $data['usr_registration_date'] = '2013-07-19 12:00:00';
+				$this->getUsersTable()->insert($data);
+				return $this->redirect()->toRoute('auth-doctrine/default', array('controller' => 'admin', 'action' => 'index'));
+			}
+		}
+		return new ViewModel(array('form' => $form));
 	}	
 	
 
@@ -43,7 +57,16 @@ class IndexController extends AbstractActionController
 	}
 	public function supprimerAction() 
 	{
-		
+
+		$id = (int) $this->params()->fromRoute('id', 0);
+		if ($id) {
+			$objectManager = $this->getEntityManager();
+			$user = $objectManager->find('Clients\Entity\Client',$id);
+			$objectManager->remove($user);
+			$objectManager->flush();
+			$this->flashMessenger()->addSuccessMessage('Client Supprimé');
+		}
+		return $this->redirect()->toRoute(NULL ,array( 'controller' => 'Index','action' => 'index'));
 	}
 	
 	/**             
