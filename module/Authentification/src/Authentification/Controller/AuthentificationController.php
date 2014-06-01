4a65695eedaf6ced8d72cf08a74daf391755ba7f
DrissGreen <driss.janid1@gmail.com>
2014-06-01 22:22:14 +0100

    Change-Id: I3655616831b044aba0f75d3a2eb389c31eca5423

--- module/Authentification/src/Authentification/Controller/AuthentificationController.php
+++ module/Authentification/src/Authentification/Controller/AuthentificationController.php
@@ -14,6 +14,9 @@ use Authentification\Form\LoginForm;
 use Authentification\Form\LoginFilter;
 use Zend\View\Model\ViewModel;
 use Zend\InputFilter\InputFilter;
+use Authentification\Form\ForgottenPasswordForm;
+use Authentification\Form\ForgottenPasswordFilter;
+use Zend\Mail\Message;
 
 class AuthentificationController extends AbstractActionController
 {
@@ -25,8 +28,9 @@ class AuthentificationController extends AbstractActionController
 	
 		$request = $this->getRequest();
 		if ($request->isPost()) {
-			$filter=new InputFilter();
-			$form->setInputFilter($filter);
+// 			$filter=new InputFilter();
+			$form->setInputFilter(new LoginFilter($this->getServiceLocator()));
+// 			$form->setInputFilter($filter);
 			$form->setData($request->getPost());
 			if ($form->isValid()) {
 				$data = $form->getData();
@@ -70,4 +74,185 @@ class AuthentificationController extends AbstractActionController
 				'messages' => $messages,
 		));
 	}
-}
+	public function logoutAction()
+	{
+		$auth = $this->getServiceLocator()->get('Zend\Authentication\AuthenticationService');
+	
+	
+	
+		if ($auth->hasIdentity()) {
+			
+			$identity = $auth->getIdentity();
+			
+		}
+		$auth->clearIdentity();
+		
+		return $this->redirect()->toRoute(NULL, array('controller' => 'Authentification', 'action' => 'login'));
+	
+	}
+	
+	// the use of controller plugin
+	public function authTestAction()
+	{
+		if ($user = $this->identity()) { // controller plugin
+			// someone is logged !
+		} else {
+			// not logged in
+		}
+	}
+	public function forgottenPasswordAction()
+	{
+		$form = new ForgottenPasswordForm();
+		$form->get('submit')->setValue('Send');
+		$request = $this->getRequest();
+		if ($request->isPost()) {
+			$form->setInputFilter(new ForgottenPasswordFilter($this->getServiceLocator()));
+			$form->setData($request->getPost());
+			if ($form->isValid()) {
+				$data = $form->getData();
+				$usrEmail = $data['usrEmail'];
+				$entityManager = $this->getServiceLocator()->get('doctrine.entitymanager.orm_default');
+				$Agent = $entityManager->getRepository('Parc\Entity\Agent')->findOneBy(array('email' => $usrEmail)); //
+				$password = $this->generatePassword();
+				$passwordHash = $this->encriptPassword($this->getStaticSalt(), $password, $Agent->getUsrPasswordSalt());
+				$this->sendPasswordByEmail($usrEmail, $password);
+				$this->flashMessenger()->addMessage($usrEmail);
+				$user->setUsrPassword($passwordHash);
+				$entityManager->persist($user);
+				$entityManager->flush();
+				return $this->redirect()->toRoute(NULL, array('controller'=>' Authentification', 'action'=>'password-change-success'));
+			}
+		}
+		return new ViewModel(array('form' => $form));
+	}
+
+	public function getStaticSalt()
+	{
+		$staticSalt = '';
+		$config = $this->getServiceLocator()->get('Config');
+		$staticSalt = $config['static_salt'];
+		return $staticSalt;
+	}
+	public function encriptPassword($staticSalt, $password, $dynamicSalt)
+	{
+		return $password = md5($staticSalt . $password . $dynamicSalt);
+	}
+	public function passwordChangeSuccessAction()
+	{
+	$usr_email = null;
+	$flashMessenger = $this->flashMessenger();
+	if ($flashMessenger->hasMessages()) {
+	foreach($flashMessenger->getMessages() as $key => $value) {
+	$usr_email .=  $value;
+	}
+	}
+	return new ViewModel(array('usr_email' => $usr_email));
+	}
+	
+	public function sendPasswordByEmail($usr_email, $password)
+	{
+		$transport = $this->getServiceLocator()->get('mail.transport');
+		$message = new Message();
+		$this->getRequest()->getServer();  //Server vars
+		$message->addTo($usr_email)
+		->addFrom('praktiki@coolcsn.com')
+		->setSubject('Your password has been changed!')
+		->setBody("Your password at  " .
+				$this->getRequest()->getServer('HTTP_ORIGIN') .
+				' has been changed. Your new password is: ' .
+				$password
+		);
+		$transport->send($message);
+	}
+	public function generatePassword($l = 8, $c = 0, $n = 0, $s = 0) {
+		// get count of all required minimum special chars
+		$count = $c + $n + $s;
+		$out = '';
+		// sanitize inputs; should be self-explanatory
+		if(!is_int($l) || !is_int($c) || !is_int($n) || !is_int($s)) {
+			trigger_error('Argument(s) not an integer', E_USER_WARNING);
+			return false;
+		}
+		elseif($l < 0 || $l > 20 || $c < 0 || $n < 0 || $s < 0) {
+			trigger_error('Argument(s) out of range', E_USER_WARNING);
+			return false;
+		}
+		elseif($c > $l) {
+			trigger_error('Number of password capitals required exceeds password length', E_USER_WARNING);
+			return false;
+		}
+		elseif($n > $l) {
+			trigger_error('Number of password numerals exceeds password length', E_USER_WARNING);
+			return false;
+		}
+		elseif($s > $l) {
+			trigger_error('Number of password capitals exceeds password length', E_USER_WARNING);
+			return false;
+		}
+		elseif($count > $l) {
+			trigger_error('Number of password special characters exceeds specified password length', E_USER_WARNING);
+			return false;
+		}
+	
+		// all inputs clean, proceed to build password
+	
+		// change these strings if you want to include or exclude possible password characters
+		$chars = "abcdefghijklmnopqrstuvwxyz";
+		$caps = strtoupper($chars);
+		$nums = "0123456789";
+		$syms = "!@#$%^&*()-+?";
+	
+		// build the base password of all lower-case letters
+		for($i = 0; $i < $l; $i++) {
+			$out .= substr($chars, mt_rand(0, strlen($chars) - 1), 1);
+		}
+	
+		// create arrays if special character(s) required
+		if($count) {
+			// split base password to array; create special chars array
+			$tmp1 = str_split($out);
+			$tmp2 = array();
+	
+			// add required special character(s) to second array
+			for($i = 0; $i < $c; $i++) {
+				array_push($tmp2, substr($caps, mt_rand(0, strlen($caps) - 1), 1));
+			}
+			for($i = 0; $i < $n; $i++) {
+				array_push($tmp2, substr($nums, mt_rand(0, strlen($nums) - 1), 1));
+			}
+			for($i = 0; $i < $s; $i++) {
+				array_push($tmp2, substr($syms, mt_rand(0, strlen($syms) - 1), 1));
+			}
+	
+			// hack off a chunk of the base password array that's as big as the special chars array
+			$tmp1 = array_slice($tmp1, 0, $l - $count);
+			// merge special character(s) array with base password array
+			$tmp1 = array_merge($tmp1, $tmp2);
+			// mix the characters up
+			shuffle($tmp1);
+			// convert to string for output
+			$out = implode('', $tmp1);
+		}
+	
+		return $out;
+	}
+	
+	public function getUsersTable()
+	{
+		if (!$this->usersTable) {
+			$sm = $this->getServiceLocator();
+			$this->usersTable = $sm->get('Auth\Model\UsersTable');
+		}
+		return $this->usersTable;
+	}
+	
+	protected $em;
+	
+	public function getEntityManager()
+	{
+		if (null === $this->em) {
+			$this->em = $this->getServiceLocator()->get('doctrine.entitymanager.orm_default');
+		}
+		return $this->em;
+	}
+	}
